3e05f784e94cd4678d30341c2613feac264ccef2
Drew Brokamp <drew.brokamp@gmail.comm>
2022-01-16 18:35:26 -0500

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Drew Brokamp on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
